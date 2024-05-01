e8bbd47997c99189712b4ebd97e7cdb8bf1ec089
whatbest121 <whatbest.ca@gmail.com>
2024-05-02 01:56:58 +0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, set_access_cookies
 from pymongo import MongoClient
 from flasgger import Swagger, swag_from
 from bson import ObjectId 
@@ -13,6 +13,17 @@ collection = db['test']
 
 swagger = Swagger(app)
 jwt = JWTManager(app)
+
+app.config['SWAGGER'] = {
+    'swagger': '2.0',
+    'securityDefinitions': {
+        'JWTAuth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    }
+}
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -42,14 +53,32 @@ def login():
     password = request.form.get('password', None)
     if username == 'admin' and password == '1234':
         access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token), 200
+        response = jsonify(access_token=access_token)
+        set_access_cookies(response, access_token)
+        return response, 200
     else:
         return jsonify({"message": "Invalid username or password"}), 401
-
 @app.route('/data', methods=['GET'])
-@jwt_required()
-@swag_from('swagger_doc.yml')
+# @jwt_required()
+@swag_from({
+    'security': [{'JWTAuth': []}],
+    'parameters': [],
+    'responses': {
+        200: {
+            'description': 'Data retrieved successfully'
+        }
+    }
+})
 def get_data():
+    """
+    Get Data Endpoint
+    ---
+    security:
+      - JWTAuth: []
+    responses:
+      200:
+        description: Data retrieved successfully
+    """
     try:
         data = list(collection.find())
         for item in data:
@@ -59,22 +88,166 @@ def get_data():
         return jsonify({"message": str(e)}), 401
 
 @app.route('/data', methods=['POST'])
-@jwt_required()
 def add_data():
-    new_data = request.get_json()
-    collection.insert_one(new_data)
-    return jsonify({"message": "Data added successfully"}), 201
-
+    """
+    Add Data Endpoint
+    ---
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            Seed_Crop_Year:
+              type: string
+            Seed_RDCSD:
+              type: string
+            Seed_RepDate:
+              type: string
+            Seed_Season:
+              type: string
+            Seed_Stock2Sale:
+              type: string
+            Seed_Variety:
+              type: string
+            Seed_Year:
+              type: string
+            Seeds_YearWeek:
+              type: string
+    responses:
+      201:
+        description: Data added successfully
+    """
+    try:
+        if request.content_type == 'application/json':
+            new_data = request.get_json()
+            result = collection.insert_one(new_data)
+            return jsonify({"message": "Data added successfully", "inserted_id": str(result.inserted_id)}), 201
+        else:
+            return jsonify({"message": "Unsupported Media Type: Request Content-Type must be 'application/json'"}), 415
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 @app.route('/data/<id>', methods=['PUT'])
-@jwt_required()
+# @jwt_required()
+@swag_from({
+    'parameters': [
+        {
+            'name': 'id',
+            'in': 'path',
+            'type': 'string',
+            'required': True,
+            'description': 'The ID of the data to update'
+        },
+        {
+            'name': 'body',
+            'in': 'body',
+            'description': 'Data to update',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'Seed_Crop _Year': {'type': 'string'},
+                    'Seed_RDCSD': {'type': 'string'},
+                    'Seed_RepDate': {'type': 'string'},
+                    'Seed_Season': {'type': 'string'},
+                    'Seed_Stock2Sale': {'type': 'string'},
+                    'Seed_Varity': {'type': 'string'},
+                    'Seed_Year': {'type': 'string'},
+                    'Seeds_YearWeek': {'type': 'string'}
+                }
+            }
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Data updated successfully'
+        }
+    }
+})
 def update_data(id):
-    updated_data = request.get_json()
-    collection.update_one({"_id": ObjectId(id)}, {"$set": updated_data})
-    return jsonify({"message": "Data updated successfully"}), 200
+    """
+    Update Data Endpoint
+    ---
+    parameters:
+      - name: id
+        in: path
+        type: string
+        required: true
+        description: The ID of the data to update
+      - name: body
+        in: body
+        description: Data to update
+        schema:
+          type: object
+          properties:
+            Seed_Crop _Year:
+              type: string
+            Seed_RDCSD:
+              type: string
+            Seed_RepDate:
+              type: string
+            Seed_Season:
+              type: string
+            Seed_Stock2Sale:
+              type: string
+            Seed_Varity:
+              type: string
+            Seed_Year:
+              type: string
+            Seeds_YearWeek:
+              type: string
+    responses:
+      200:
+        description: Data updated successfully
+    """
+    try:
+        if request.content_type == 'application/json':  # Check Content-Type
+            updated_data = request.get_json()
+
+            existing_data = collection.find_one({"_id": ObjectId(id)})
+
+            updated_fields = {k: v for k, v in updated_data.items() if k in existing_data}
+
+            collection.update_one({"_id": ObjectId(id)}, {"$set": updated_fields})
+            return jsonify({"message": "Data updated successfully"}), 200
+        else:
+            return jsonify({"message": "Unsupported Media Type: Request Content-Type must be 'application/json'"}), 415
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
 
 @app.route('/data/<id>', methods=['DELETE'])
-@jwt_required()
+# @jwt_required()
+@swag_from({
+    'parameters': [
+        {
+            'name': 'id',
+            'in': 'path',
+            'type': 'string',
+            'required': True,
+            'description': 'The ID of the data to delete'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Data deleted successfully'
+        }
+    }
+})
 def delete_data(id):
+    """
+    Delete Data Endpoint
+    ---
+    parameters:
+      - name: id
+        in: path
+        type: string
+        required: true
+        description: The ID of the data to delete
+    responses:
+      200:
+        description: Data deleted successfully
+    """
     collection.delete_one({"_id": ObjectId(id)})
     return jsonify({"message": "Data deleted successfully"}), 200
 
